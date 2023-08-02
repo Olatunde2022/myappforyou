be2e2294bca7a81e3f6cc7fe7059f8fd971e4758
unknown <ojekab2580@gmail.com>
2023-08-03 00:12:04 +0100

    App UI is ready

---
+++ src/utils/utils.js
@@ -0,0 +1,11 @@
+
+
+export const returnFirst20CharactersOfString = (str) =>{
+    const strArr = str.split("");
+    let strToReturn = ''
+    for (let i = 0; i < 20; i++){
+        strToReturn = strArr
+        // return (strArr)
+        console.log(strArr);
+    }
+}
